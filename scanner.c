b9d42eabdc3a8ef8a3ab747dd83a3aa887354fe6
Sanjay <sanjaysankar2030@gmail.com>
2025-07-15 22:55:32 +0530

    Started the lexical grammer

--- scanner.c
+++ scanner.c
@@ -3,6 +3,7 @@
 
 #include "scanner.h"
 
+#include <math.h>
 #include <stdbool.h>
 #include <string.h>
 
@@ -14,9 +15,43 @@ typedef struct {
 
 Scanner scanner;
 
-static bool isAtEnd() { return *scanner.current == '\0'; }
+static char advance() {
+  scanner.current++;
+  return scanner.current[-1];
+}
 
+static bool isAtEnd() {
+  return *scanner.current == '\0';
+}  // Leaves null
+   // values
+
+static bool match(char expr) {
+  if (isAtEnd()) return false;
+  if (*scanner.current != expr) return false;
+  scanner.current++;
+  return true;
+}
+static char peak() { return *scanner.current; }
+static void skipWhitespace() {
+  for (;;) {
+    char expr = peak();
+    switch (expr) {
+      case ' ':
+      case '\r':
+      case '\t':
+        advance();
+        break;
+      case '\n':
+        scanner.lines++;
+        advance();
+        break;
+      default:
+        return;
+    }
+  }
+}
 static Token giveToken(TokenType type) {
+  // generates token
   Token token;
   token.t_type = type;
   token.start = scanner.start;
@@ -37,11 +72,44 @@ void initScanner(const char* source) {
   scanner.current = source;
   scanner.lines = 1;
 }
+// Scans and tokenises using the give Token function
 Token scanToken() {
+  skipWhitespace();
   scanner.start = scanner.current;
   if (isAtEnd()) return giveToken(TOKEN_EOF);
-
+  char c = advance();
+  switch (c) {
+    case '(':
+      return giveToken(TOKEN_LEFT_PAREN);
+    case ')':
+      return giveToken(TOKEN_RIGHT_PAREN);
+    case '{':
+      return giveToken(TOKEN_LEFT_BRACE);
+    case '}':
+      return giveToken(TOKEN_RIGHT_BRACE);
+    case ';':
+      return giveToken(TOKEN_SEMICOLON);
+    case ',':
+      return giveToken(TOKEN_COMMA);
+    case '.':
+      return giveToken(TOKEN_DOT);
+    case '-':
+      return giveToken(TOKEN_MINUS);
+    case '+':
+      return giveToken(TOKEN_PLUS);
+    case '/':
+      return giveToken(TOKEN_SLASH);
+    case '*':
+      return giveToken(TOKEN_STAR);
+    case '!':
+      return giveToken(match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+    case '<':
+      return giveToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+    case '=':
+      return giveToken(match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+    case '>':
+      return giveToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+  }
   return errorToken("Unexpected char .");
 }
-
 #endif
